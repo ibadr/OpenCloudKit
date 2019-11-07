076c7ac172f32a2e8398d43580e1cf9839a76d84
ibadr <ibadr@users.noreply.github.com>
2019-11-07 21:00:45 +0200

    Update CKQuery.swift

--- Sources/CKQuery.swift
+++ Sources/CKQuery.swift
@@ -52,7 +52,7 @@ public class CKQuery: CKCodable {
         }).bridge()
         
         // Create Sort Descriptor Dictionaries
-        queryDictionary["sortBy"] = sortDescriptors.flatMap { (sortDescriptor) -> [String: Any]? in
+        queryDictionary["sortBy"] = sortDescriptors.compactMap { (sortDescriptor) -> [String: Any]? in
             
             if let fieldName = sortDescriptor.key {
                 var sortDescriptionDictionary: [String: Any] =  [CKSortDescriptorDictionary.fieldName: fieldName.bridge(),
